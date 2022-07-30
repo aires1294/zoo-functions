782b4fef8b66b3470f72f0faea1c2fce5313ff86
aires1294 <pedrotja.engenharia@gmail.com>
2022-07-29 23:28:07 -0300

    item 2 FEITOOO

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return data.species
+      .find((specie) => animal.includes(specie.name)).residents
+      .every((older) => older.age >= age);
 }
-
+console.log(getAnimalsOlderThan('otters', 7));
 module.exports = getAnimalsOlderThan;
