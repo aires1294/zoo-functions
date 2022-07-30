cde283dfef640b0d09f3d8a8f9f1842c49036618
aires1294 <pedrotja.engenharia@gmail.com>
2022-07-29 23:59:30 -0300

    exercicio 3 FEITOOO

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,11 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+  return data.employees.find((nome) => employeeName
+    .includes(nome.firstName) || employeeName.includes(nome.lastName));
 }
-
+console.log(getEmployeeByName('pedro'));
 module.exports = getEmployeeByName;
