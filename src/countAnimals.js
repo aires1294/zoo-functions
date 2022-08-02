3a7fc472372c06fb7a734bda03a13664f1228e9d
aires1294 <pedrotja.engenharia@gmail.com>
2022-08-02 18:25:38 -0300

    item 5 feito

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,9 +1,30 @@
+// const { species } = require('../data/zoo_data');
+// const data = require('../data/zoo_data');
+
+// function countAnimals(animal) {
+//   if (!animal) {
+//     return species.map((countAnimal) => `${countAnimal.name} : ${countAnimal.residents.length}`);
+//   }
+//   if (animal.specie && animal.sex) {
+//     return species.find((animalSex) => animalSex.name === animal.specie).residents
+//       .filter((animalSexOnly) => animalSexOnly.sex === animal.sex).length;
+//   }
+//   if (animal.specie) {
+//     return species.find((specificAnimal) => specificAnimal.name === animal.specie).residents.length;
+//   }
+// }
+// console.log(countAnimals());
+// module.exports = countAnimals;
+
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
   if (!animal) {
-    return species.map((countAnimal) => `${countAnimal.name} : ${countAnimal.residents.length}`);
+    return species.reduce((acc, currentValue) => {
+      acc[currentValue.name] = currentValue.residents.length;
+      return acc;
+    }, {});
   }
   if (animal.specie && animal.sex) {
     return species.find((animalSex) => animalSex.name === animal.specie).residents
@@ -13,5 +34,5 @@ function countAnimals(animal) {
     return species.find((specificAnimal) => specificAnimal.name === animal.specie).residents.length;
   }
 }
-console.log(countAnimals({ specie: 'lions', sex: 'male' }));
+console.log(countAnimals());
 module.exports = countAnimals;
