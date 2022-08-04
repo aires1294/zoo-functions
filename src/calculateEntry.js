c5a354afd28c394cb76d8c3e36685f839318fc30
aires1294 <pedrotja.engenharia@gmail.com>
2022-08-04 19:35:43 -0300

    item 8 FEITOO

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,13 +1,28 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const personByAge = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach((element) => {
+    if (element.age < 18) {
+      personByAge.child += 1;
+    } if (element.age < 50 && element.age >= 18) {
+      personByAge.adult += 1;
+    } if (element.age >= 50) {
+      personByAge.senior += 1;
+    }
+  });
+  return personByAge;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || entrants.length === undefined) {
+    return 0;
+  }
+  const value = data.prices
+  const { child, adult, senior } = countEntrants(entrants);
+  let result = 0;
+
+  result = (child * value.child) + (adult * value.adult) + (senior * value.senior);
+  return result;
 }
-
 module.exports = { calculateEntry, countEntrants };
-
-// teste
