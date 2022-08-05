e3519e4b39968aa54b1a2ea7cf6ba76aa994e0e1
aires1294 <pedrotja.engenharia@gmail.com>
2022-08-05 02:02:43 -0300

    item 10 feitoo

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,11 +1,26 @@
 const data = require('../data/zoo_data');
 
 const { employees } = data;
+const { species } = data;
 
 function getOldestFromFirstSpecies(id) {
-  const idEmployee = employees.find((funcionario) => funcionario.id === id).responsibleFor[0];
-  return idEmployee;
-}
+  const idEmployee = employees.find((funcionario) => funcionario.id === id).responsibleFor[0];  
+  // const idAnimal = idEmployee.reduce((older, actual) => {
+  //   if (older.residents.age > actual.residents.age) {
+  //     return older;
+  //   }
+  //   return actual;
+  // });
+  const idAnimal = species.find((animal) => animal.id === idEmployee).residents
+    .reduce((older, actual) => {
+      if (older.age > actual.age) {
+        return older;
+      }
+      return actual;
+    });
 
-console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
+  return [idAnimal.name, idAnimal.sex, idAnimal.age];
+  // return [idAnimal];
+}
+// console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
