3aa22b703cda936a96166b5336f5602cb95dc879
aires1294 <pedrotja.engenharia@gmail.com>
2022-08-06 19:21:48 -0300

    item 12 e 13

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,21 +1,3 @@
-// const { species } = require('../data/zoo_data');
-// const data = require('../data/zoo_data');
-
-// function countAnimals(animal) {
-//   if (!animal) {
-//     return species.map((countAnimal) => `${countAnimal.name} : ${countAnimal.residents.length}`);
-//   }
-//   if (animal.specie && animal.sex) {
-//     return species.find((animalSex) => animalSex.name === animal.specie).residents
-//       .filter((animalSexOnly) => animalSexOnly.sex === animal.sex).length;
-//   }
-//   if (animal.specie) {
-//     return species.find((specificAnimal) => specificAnimal.name === animal.specie).residents.length;
-//   }
-// }
-// console.log(countAnimals());
-// module.exports = countAnimals;
-
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
--- test/getOpeningHours.test.js
+++ test/getOpeningHours.test.js
@@ -1,3 +1,46 @@
 const getOpeningHours = require('../src/getOpeningHours');
 
 describe('Testes da função getOpeningHours', () => {});
+test('Teste quando nao se passa argumentos', () => {
+  expect(getOpeningHours()).toEqual({
+    Tuesday: { open: 8, close: 6 },
+    Wednesday: { open: 8, close: 6 },
+    Thursday: { open: 10, close: 8 },
+    Friday: { open: 10, close: 8 },
+    Saturday: { open: 8, close: 10 },
+    Sunday: { open: 8, close: 8 },
+    Monday: { open: 0, close: 0 },
+  });
+});
+
+test('Para os argumentos Monday e 09:00-AM', () => {
+  expect(getOpeningHours('Monday', '09:00-AM')).toBe('The zoo is closed');
+});
+
+test('Para os argumentos Tuesday e 09:00-AM', () => {
+  expect(getOpeningHours('Tuesday', '09:00-AM')).toBe('The zoo is open');
+});
+
+test('Para os argumentos Wednesday e 09:00-PM', () => {
+  expect(getOpeningHours('Wednesday', '09:00-PM')).toBe('The zoo is closed');
+});
+
+test('Para os argumentos Thu e 09:00-AM', () => {
+  expect(() => getOpeningHours('Thu', '09:00-AM')).toThrowError('The day must be valid. Example: Monday');
+});
+// fazer esse de cima de outro jeito
+// describe('Testes da função HandlerElephants', () => {
+//     it('retorna a quantidade de elefantes', () => {
+//       const actual = handlerElephants('count');
+//       const expected = 4;
+//       expect(actual).toBe(expected);
+//     });
+//   });
+
+test('Para os argumentos Friday e 09:00-ZM', () => {
+  expect(() => getOpeningHours('Friday', '09:00-ZM')).toThrowError('The abbreviation must be \'AM\' or \'PM\'');
+});
+
+test('Para os argumentos Saturday e C9:00-AM', () => {
+  expect(() => getOpeningHours('Saturday', 'C9:00-AM')).toThrowError('The hour should represent a number');
+});
